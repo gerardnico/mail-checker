ca5ef9a18ba842161357b1924eddae0ab626807a
Nicolas GERARD <gerardnico@gmail.com>
2025-03-25 16:01:24 +0100

    build: added build tool in nix-env

--- default.nix
+++ default.nix
@@ -2,8 +2,7 @@
 # The main nix script
 # Tuto: https://nix.dev/guides/recipes/sharing-dependencies
 let
-  # <nixpkgs> refers to NIX_PATH
-  # export NIX_PATH="nixpkgs=channel:nixos-24.11"
+  # <nixpkgs> refers to the nixpkgs channel `channel:nixos-24.11`
   pkgs = import <nixpkgs> { config = {}; overlays = []; };
   # 2.7.1 for semantic release https://semantic-release.gitbook.io/semantic-release/support/git-version
   # 2.9.0 for hooks https://stackoverflow.com/questions/39332407/git-hooks-applying-git-config-core-hookspath
@@ -26,31 +25,13 @@ assert pkgs.lib.versionAtLeast pkgs.git.version gitMinVersion || throw "Git vers
         nodejs
         nodePackages."@commitlint/cli"
         nodePackages."@commitlint/config-conventional"
-        jreleaser-cli
       ];
       # Take the package’s dependencies into the environment with inputsFrom
       inputsFrom = [ mail-checker ];
       shellHook = ''
-          echo "Hallo from Nix"
-
+          echo "Hallo from Nix. Build Tools:"
           echo "Git Version        : $(git --version)"
-
           echo "Go Version         : $(go version)"
-
-          echo "JReleaser version  : $(jreleaser --version | grep jreleaser | cut -d ' ' -f '2-')"
-
-          echo "CommitLint version : $(commitlint -version)"
-
-          # Node for commitlint
-          export NODE_BIN="${pkgs.nodejs}/bin/node"
-          #echo "Node version: $(node --version)"
-          #echo "NPM version: $(npm --version)"
-          #echo "NPX version: $(npx --version)"
-
-          #mkdir -p $out/bin
-          #export COREPACK_ENABLE_DOWNLOAD_PROMPT=0 && corepack enable --install-directory=$out/bin
-          #export PATH="$PATH:$out/bin"
-          #echo "Yarn version: $(yarn --version)"
           '';
 
      };
