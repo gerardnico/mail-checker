f728b65cdb8fdd7fcec7adab8054e6a20368a6b4
Nicolas GERARD <gerardnico@gmail.com>
2025-03-26 10:59:11 +0100

    build: release-go on the go

--- default.nix
+++ default.nix
@@ -1,13 +1,9 @@
-# default.nix
-# The main nix script
-# Tuto: https://nix.dev/guides/recipes/sharing-dependencies
+# default.nix, the main nix script
 let
   # <nixpkgs> refers to the nixpkgs channel `channel:nixos-24.11`
   pkgs = import <nixpkgs> { config = {}; overlays = []; };
-  # 2.7.1 for semantic release https://semantic-release.gitbook.io/semantic-release/support/git-version
   # 2.9.0 for hooks https://stackoverflow.com/questions/39332407/git-hooks-applying-git-config-core-hookspath
   gitMinVersion = "2.9.0";
-  mail-checker = pkgs.callPackage ./build.nix { go = pkgs.go_1_23; };
 in
 
 assert pkgs.lib.versionAtLeast pkgs.git.version gitMinVersion || throw "Git version ${pkgs.git.version} is less than required minimum ${gitMinVersion} for git hooks";
@@ -15,9 +11,6 @@ assert pkgs.lib.versionAtLeast pkgs.git.version gitMinVersion || throw "Git vers
 # Returned object
 {
 
-  # Create a mail-checker
-  # that you can run with nix-build -A mail-checker
-  inherit mail-checker;
   # Create a shell function (used in shell.nix)
   shell = pkgs.mkShellNoCC {
       packages = with pkgs; [
